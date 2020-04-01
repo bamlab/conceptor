e21089c97c9dc0f788dc5fec23b48e2d829ecdd9
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2020-04-02 01:03:12 +0200

    🚧  Display basic graph
    https://blog.js.cytoscape.org/2016/05/24/getting-started/

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,57 @@
 import * as vscode from 'vscode';
 import { parse } from 'doctrine';
 
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conceptor Graph</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.14.1/cytoscape.min.js"></script>
+</head>
+<style>
+    #cy {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0px;
+        left: 0px;
+    }
+</style>
+<body>
+    <div id="cy"></div>
+    <script>
+      var cy = cytoscape({
+        container: document.getElementById('cy'),
+        elements: [
+          { data: { id: 'a' } },
+          { data: { id: 'b' } },
+          {
+            data: {
+              id: 'ab',
+              source: 'a',
+              target: 'b'
+            }
+          }
+        ],
+        style: [
+          {
+              selector: 'node',
+              style: {
+                  shape: 'round-rectangle',
+                  'background-color': 'white',
+                  label: 'data(id)',
+                  'text-halign': 'center',
+                  'text-valign': 'center'
+              }
+          }]    
+      });
+    </script>
+</body>
+</html>`;
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -19,35 +70,38 @@ export function activate(context: vscode.ExtensionContext) {
     // Display a message box to the user
     vscode.window.showInformationMessage('🚀Launching Conceptor ...');
 
-    vscode.window.createWebviewPanel(
+    const panel = vscode.window.createWebviewPanel(
       'conceptor.preview',
       'Conceptor',
       vscode.ViewColumn.Eight,
+      { enableScripts: true },
     );
 
-    vscode.workspace
-      .findFiles('**/*.{ts,js,tsx,jsx}', '**/node_modules/**')
-      .then((fileUris: vscode.Uri[]) => {
-        fileUris.forEach((fileUri: vscode.Uri) => {
-          vscode.workspace
-            .openTextDocument(fileUri.path)
-            .then((projectDocument: vscode.TextDocument) =>
-              projectDocument.getText(),
-            )
-            .then((text: string) => {
-              // TODO: Use a cleaner way to ignore file body and keep header
-              const [header, body] = text.split('**/');
-              if (!body) {
-                // the file actually has no header so we break
-                return;
-              }
-              const ast = parse(header, {
-                unwrap: true,
-              });
-              console.log(ast);
-            });
-        });
-      });
+    panel.webview.html = getWebviewContent();
+
+    // vscode.workspace
+    //   .findFiles('**/*.{ts,js,tsx,jsx}', '**/node_modules/**')
+    //   .then((fileUris: vscode.Uri[]) => {
+    //     fileUris.forEach((fileUri: vscode.Uri) => {
+    //       vscode.workspace
+    //         .openTextDocument(fileUri.path)
+    //         .then((projectDocument: vscode.TextDocument) =>
+    //           projectDocument.getText(),
+    //         )
+    //         .then((text: string) => {
+    //           // TODO: Use a cleaner way to ignore file body and keep header
+    //           const [header, body] = text.split('**/');
+    //           if (!body) {
+    //             // the file actually has no header so we break
+    //             return;
+    //           }
+    //           const ast = parse(header, {
+    //             unwrap: true,
+    //           });
+    //           console.log(ast);
+    //         });
+    //     });
+    //   });
   });
 
   context.subscriptions.push(disposable);
