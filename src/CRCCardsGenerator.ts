246209209c06cfdf824663b9c9ff6227437504c2
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2020-04-19 01:01:15 +0200

    🔨  Move readFile utility into a FileSystem utils

--- src/CRCCardsGenerator.ts
+++ src/CRCCardsGenerator.ts
@@ -6,18 +6,14 @@
 import * as vscode from 'vscode';
 import { without } from 'lodash';
 import { CRCParser } from './CRCParser';
+import { readFile } from './utils/FileSystem';
 
 export class CRCCardsGenerator {
-  private static readFile = async (fileUri: vscode.Uri) => {
-    const document = await vscode.workspace.openTextDocument(fileUri.path);
-    return document.getText();
-  };
-
   public static generateCRCCards = async (fileUris: vscode.Uri[]) => {
     return without(
       await Promise.all(
         fileUris.map(async (fileUri: vscode.Uri) => {
-          const documentText = await CRCCardsGenerator.readFile(fileUri);
+          const documentText = await readFile(fileUri);
 
           return CRCParser.extractCRCCard(documentText);
         }),
