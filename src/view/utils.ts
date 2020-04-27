50b56201c78cc3cc9cb355b7a3a452e61a0329d0
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2020-04-27 19:30:58 +0200

    🔨  Refactor templates to use .ejs extension to increase  readability

--- src/view/utils.ts
+++ src/view/utils.ts
@@ -1,12 +1,18 @@
 import { renderFile } from 'ejs';
+import * as path from 'path';
 
-export const compileTemplate = (templatePath: string, data: Object) =>
+export const compileTemplate = (templateName: string, data: Object) =>
   new Promise<string>(async (resolve, reject) => {
-    renderFile(templatePath, data, {}, (err, str) => {
-      if (err) {
-        console.warn(err);
-        reject(err);
-      }
-      resolve(str);
-    });
+    renderFile(
+      path.resolve(__dirname, `../templates/${templateName}.ejs`),
+      data,
+      {},
+      (err, str) => {
+        if (err) {
+          console.warn(err);
+          reject(err);
+        }
+        resolve(str);
+      },
+    );
   });
