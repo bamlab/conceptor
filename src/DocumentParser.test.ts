7d0a331f9185c58f0138524ac2b0285906f6bf3e
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2020-04-29 01:14:03 +0200

    ✅  Test DocumentParser.parse with skipUnannotated option at true

--- src/DocumentParser.test.ts
+++ src/DocumentParser.test.ts
@@ -1,7 +1,19 @@
 import * as assert from 'assert';
+import { DocumentParser } from './DocumentParser';
+import * as sinon from 'sinon';
+import { Uri } from 'vscode';
+import * as FileSystem from './utils/FileSystem';
+
+const readFileStub = sinon.stub(FileSystem, 'readFile');
 
 suite('DocumentParser', () => {
-  test('should pass', () => {
-    assert.equal(true, true);
+  suite('parse', () => {
+    test('should return null when the "skipUnannotated" option is provided and the document has no header', async () => {
+      readFileStub.callsFake(async () => 'Document with no header');
+      const document = await DocumentParser.parse(Uri.file('/my/path'), {
+        skipUnannotated: true,
+      });
+      assert.equal(document, null);
+    });
   });
 });
