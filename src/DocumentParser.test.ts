3d2a13a810eade97687e1bc18a2d95ffb5cd3fc4
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2020-05-11 01:18:28 +0200

    :white_check_mark: (DocumentParser) Fix DocumentParser Test

--- src/DocumentParser.test.ts
+++ src/DocumentParser.test.ts
@@ -4,6 +4,7 @@ import * as sinon from 'sinon';
 import { Uri } from 'vscode';
 
 import * as FileSystem from './utils/FileSystem';
+import { Dependency } from './typings/model';
 
 const readFileStub = sinon.stub(FileSystem, 'readFile');
 
@@ -37,7 +38,7 @@ const imports = [
     importList: ['One', 'Two', 'Three', 'Four', 'Five'],
     modulePath: 'multiline-import-package',
     originalMatch:
-      "import {\n One,\n Two,\n Three,\n Four,\n Five,\n } from 'multiline-import-package'",
+      "import {\n One,\n Two,\n Three,\n Four,\n Five\n } from 'multiline-import-package'",
   },
   {
     importList: ['Foo'],
@@ -45,8 +46,19 @@ const imports = [
     originalMatch: "import { Foo } from './bar'",
   },
 ];
-const header = `/**\n* @${annotation.tags[0].title} ${annotation.tags[0].name}\n* @${annotation.tags[1].title} ${annotation.tags[1].description}\n**/`;
-const body = `\n${imports[0].originalMatch}';\n${imports[1].originalMatch};\n${imports[2].originalMatch};\n${imports[3].originalMatch};\n\nexport class ${name} {\n public static doStuff = async (param: string) => {\n   return \`Do somoe stuff with \${param}\`\n };\n}\n`;
+const dependencies: Dependency[] = imports.map(
+  ({ originalMatch, modulePath, importList }: Import) => ({
+    rawImportStatement: `${originalMatch};`,
+    importList,
+    path: {
+      raw: modulePath,
+      absolute: modulePath === './bar' ? '/my/path/to/bar' : undefined,
+    },
+  }),
+);
+const header = `/**\n* @${annotation.tags[0].title} ${annotation.tags[0].name}\n* @${annotation.tags[1].title} ${annotation.tags[1].description}\n*/`;
+const body = `\n${imports[0].originalMatch};\n${imports[1].originalMatch};\n${imports[2].originalMatch};\n${imports[3].originalMatch};\n\nexport class ${name} {\n public static doStuff = async (param: string) => {\n   return \`Do somoe stuff with \${param}\`\n };\n}\n`;
+
 const annotatedDocument = `${header}${body}`;
 const unannotatedDocument = body;
 
@@ -74,9 +86,9 @@ suite('DocumentParser', () => {
         name,
         annotation,
         body,
-        imports,
+        dependencies,
       };
-      assert.notStrictEqual(document, expectedParsedDocument);
+      assert.deepStrictEqual(document, expectedParsedDocument);
     });
     test('should return parsed document when the document is annotated when the "skipUnannotated" option is not provided', async () => {
       readFileStub.callsFake(async () => annotatedDocument);
@@ -86,13 +98,14 @@ suite('DocumentParser', () => {
           skipUnannotated: false,
         },
       );
+
       const expectedParsedDocument = {
         name,
         annotation,
         body,
-        imports,
+        dependencies,
       };
-      assert.notStrictEqual(document, expectedParsedDocument);
+      assert.deepStrictEqual(document, expectedParsedDocument);
     });
   });
 });
